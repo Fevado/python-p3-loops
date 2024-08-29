ed2b13e9230839e51789012619e3722dd491fd58
favoured mwange <mwangefavoured@gmail.com>
2024-08-29 22:58:15 +0300

    Practices on python looping

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,30 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    # pass
+    i = 11
+    while i>1:
+        i -= 1
+        print (i)
+        print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    # pass
+    # squared = [ ]
+    # for i in int_list:
+    #     squared.append(i*i) 
+    return [num ** 2 for num in int_list]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    # pass
+     results= [
+         'FizzBuzz' if num % 3 == 0 and num % 5 == 0
+          else 'Fizz' if num % 3 == 0
+          else 'Buzz' if num % 5 == 0
+          else num
+          for num in range(1, 101)
+    ]
+     for result in results:
+        print(result)
